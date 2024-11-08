7006c6693cff6385df3545425a67cdfcd2a05b36
DaviGN <davi.gnascimento@souunit.com.br>
2024-11-08 17:14:03 -0300

    display Buttons

--- script.js
+++ script.js
@@ -3,15 +3,31 @@ const indicator = document.getElementById("result");
 const whiteDot = document.getElementById("branco");
 const whiteCar = document.getElementById("white");
 const redDot = document.getElementById("vermelho");
+const redCar = document.getElementById("red");
 
-whiteDot.addEventListener("click", () => {
+const buttons = document.querySelectorAll(".btn");
+const reset = document.getElementById("resetar");
+const accelerate = document.getElementById("acelerar");
+const slow = document.getElementById("desacelerar");
+
+const displayButtons = () => {
+    buttons.forEach( button => {
+        button.style.display = "inline";
+    });
+};
+
+whiteDot.addEventListener("click", whiteClick = () => {
     indicator.innerHTML = "Carro Branco";
     body.style.backgroundColor = "white";
     body.style.color = "black";
+    displayButtons();
 });
+whiteCar.addEventListener("click", whiteClick)
 
 redDot.addEventListener("click", redClick = () => {
     indicator.innerHTML = "Carro Vermelho";
     body.style.backgroundColor = "red";
     body.style.color = "white";
+    displayButtons();
 });
+redCar.addEventListener("click", redClick);
