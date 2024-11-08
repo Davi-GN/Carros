8a6ae6d92016c0c3514842ef3023e533ae558a7c
DaviGN <davi.gnascimento@souunit.com.br>
2024-11-08 16:42:11 -0300

    Alteração de cor de Fonte em função do carro selecionado

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const body = document.querySelector("body");
+const indicator = document.getElementById("result");
 const whiteDot = document.getElementById("branco");
+const whiteCar = document.getElementById("white");
 const redDot = document.getElementById("vermelho");
 
 whiteDot.addEventListener("click", () => {
-    console.log("asda")
+    indicator.innerHTML = "Carro Branco";
     body.style.backgroundColor = "white";
+    body.style.color = "black";
 });
 
-redDot.addEventListener("click", () => {
-    console.log("asda")
+redDot.addEventListener("click", redClick = () => {
+    indicator.innerHTML = "Carro Vermelho";
     body.style.backgroundColor = "red";
+    body.style.color = "white";
 });
